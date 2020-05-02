c5b3d893e3b7715e206864a0be3c600c5c96ba4c
wenner <nelsondiasdemedeiros@gmail.com>
2020-05-02 13:12:32 -0300

    chore: add ui and redux-async-cep

--- redux-async-cep/src/pages/home/index.js
+++ redux-async-cep/src/pages/home/index.js
@@ -3,19 +3,52 @@ import './style.css';
 
 import { connect } from 'react-redux';
 
-import SearchCep from '../../components/common/search-cep'
+import SearchCep from '../../components/common/search-cep';
+import Api from '../../services/Api';
 
 
 class HomeContainer extends Component {
 
   constructor() {
     super();
+
+    this.state = {
+      address: '',
+      city: '',
+      district: '',
+      state: '',
+      code: '',
+      status: 0,
+      ok: true,
+      isFaching: false
+    }
+  }
+
+  async componentDidMount() {
+  
+  }
+
+  handlerSubmit = async (event) => {
+    event.preventDefault();
+    this.setState({isFaching: true});
+    const cep = event.target.cep.value.toString();
+    const { data } = await Api.ApiCep.get(`/cep.json?code=${ cep }`);
+
+    setTimeout(() => {
+      this.setState({isFaching: false});
+      this.setState(data);
+    }, 5000);
+   
+    //event.target.cep.value = '';
   }
 
   render() {
     return (
       <div className="container" style={{ paddingTop: 120 }}>
-        <SearchCep />
+        <SearchCep
+          { ...this.state } 
+          handlerSubmit={ this.handlerSubmit }  
+        />
       </div>
     )
   }
